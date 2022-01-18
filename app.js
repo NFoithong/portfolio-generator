de519051107150b2d95eece1a93a4f01e98b02a3
NFoithong <n.foithong1983@gmail.com>
2022-01-17 22:18:41 -0500

    first inqiurer feature/questions

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello Node!");
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('portfolio complete! Checkout index.html to see the output!')
+// })
+// console.log(inquirer);
+
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
